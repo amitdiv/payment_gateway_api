1647fb3a9d67c95b60f23ef3cc14d2b4ce8aa0f3
amitdiv <37323836+amitdiv@users.noreply.github.com>
2021-01-19 21:19:28 +0530

    task: api development for payment process modification: added loggers

--- processPayment.py
+++ processPayment.py
@@ -1,5 +1,6 @@
 import datetime
 import configparser
+import logging
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_restful import reqparse
@@ -8,6 +9,7 @@ api = Api(app)
 
 
 class Pay(Resource):
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(lineno)d :: %(message)s',filename='call_execution.log')
 
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
@@ -21,33 +23,43 @@ class Pay(Resource):
         self.curr_time=str(datetime.datetime.today().strftime('%Y-%m-%d'))
 
     def get(self):
+        logging.info("Invoking GET request to api")
         try:
             ret_val = 0
             config = None
             args = self.reqparse.parse_args()
             args['expiry'] = str(datetime.datetime.strptime(args['expiry'], "%Y-%m-%d").date())
             if len(args['number'])!=16 or args['expiry']<self.curr_time or not args['owner'] or (not args['amount'] or args['amount']<1):
+                logging.error("One of the parameters in api call has not fulfilled the requirement. Exiting execution")
                 return self.return_400()
             elif not args['secure_code'] or len(args['secure_code'])!=3:
+                logging.warning("Either secure_code provided is null or incorrect. Please check")
                 return "Please provide a valid secure_code to process the payment"
             elif args['amount']<20:
+                logging.info("Payment is getting processed through cheappaymentgateway")
                 ret_val,config=self.cheappaymentgateway(args['amount'],args['owner'])
             elif args['amount']>20 and args['amount']<501:
+                logging.info("Payment is getting processed through expensivepaymentgateway")
                 ret_val,config=self.expensivepaymentgateway(args['amount'],args['owner'])
                 if ret_val!=1:
+                    logging.info("Payment is getting processed through cheappaymentgateway because expensivepaymentgateway is not responding")
                     ret_val,config = self.cheappaymentgateway(args['amount'],args['owner'])
             elif args['amount']>500:
                 for i in range(3):
+                    logging.info("Payment is getting processed through premiumpaymentgateway")
                     ret_val,config=self.premiumpaymentgateway(args['amount'],args['owner'])
                     if ret_val==1:
                         break
             else:
+                logging.error("The request made was invalid hence api failed to respond. Exiting execution")
                 return self.return_500()
             with open("mocked_payment_confirmation.conf","w") as file:
                 config.write(file)
             if ret_val==1:
+                logging.info("Processing of payment completed successfully.")
                 return self.return_200()
             else:
+                logging.error("Processing of payment failed")
                 return self.return_400()
         except Exception as e:
             return "An Exception occurred : {0}".format(e)
